001439fae3d3802acc8c75ae4a722d1682db0d16
Georgios Basinas <geobasinas@gmail.com>
2024-10-13 23:31:19 +0300

    added security headers

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,27 @@
 /** @type {import('next').NextConfig} */
+
+
+// security headers
+const securityHeaders = [
+  {
+    key: 'X-XSS-Protection',
+    value: '1; mode=block'
+  },
+  {
+    key: 'X-Frame-Options',
+    value: 'SAMEORIGIN'
+  },
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff'
+  },
+  {
+    key: 'Referrer-Policy',
+    value: 'origin-when-cross-origin'
+  },
+  // Add Content Security Policy (CSP) as needed
+]
+
 const nextConfig = {
     reactStrictMode: true,
     images: {
@@ -24,6 +47,14 @@ const nextConfig = {
         }
         // Important: return the modified config
         return config
+    },
+    async headers() {
+      return [
+        {
+          source: '/:path*',
+          headers: securityHeaders,
+        },
+      ]
     }
 };
 
